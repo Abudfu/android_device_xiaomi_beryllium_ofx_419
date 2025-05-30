2c99116c01df71a4c32e3adc3f388174ec91da0c
ferrpp <42369367+Abudfu@users.noreply.github.com>
2025-05-31 03:05:54 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -48,17 +48,14 @@ PRODUCT_PACKAGES += \
 #    android.system.keystore2
 
 TARGET_RECOVERY_DEVICE_MODULES += \
-	libandroidicu \
-	libion \
-	vendor.display.config@1.0 \
-	vendor.display.config@2.0 \
-	libdisplayconfig.qti
+    libion \
+    libxml2 \
+    libicuuc
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so
 
 #PRODUCT_COPY_FILES += \
 #    $(OUT_DIR)/target/product/beryllium/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
