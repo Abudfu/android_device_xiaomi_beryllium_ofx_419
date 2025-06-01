61000690932cd0b81c77c9347cb09078cce74df3
ferrpp <42369367+Abudfu@users.noreply.github.com>
2025-06-02 04:41:46 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,36 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from the common Open Source product configuration
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# GSI
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 LOCAL_PATH := device/xiaomi/beryllium
-
-# OTA
-# TARGET_OTA_ASSERT_DEVICE := beryllium
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# APEX
-PRODUCT_COMPRESSED_APEX := false
-
-TW_EXCLUDE_APEX := true
-
-# Enable updating of APEXes
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0.vendor
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
 
 # shipping API
 PRODUCT_SHIPPING_API_LEVEL := 27
@@ -57,16 +32,9 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so
 
-#PRODUCT_COPY_FILES += \
-#    $(OUT_DIR)/target/product/beryllium/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
-
 # for Android 11+ manifests
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys-intf/display
-
-# OEM otacert
-#PRODUCT_EXTRA_RECOVERY_KEYS += \
-#    vendor/recovery/security/miui
 
 # copy recovery/root/ from the common directory
 PRODUCT_COPY_FILES += \
@@ -77,7 +45,3 @@ ifneq ($(wildcard $(DEVICE_PATH)/recovery/root/.),)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root/,$(TARGET_COPY_OUT_RECOVERY)/root/)
 endif
-
-# Inherit from the device-specific device.mk (if it exists) as the last in the chain
-#$(call inherit-product-if-exists, $(DEVICE_PATH)/device.mk)
-
