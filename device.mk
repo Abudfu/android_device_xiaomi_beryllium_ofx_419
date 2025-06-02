9b66b03d31e7079f91bf796be49b910912f61679
ferrpp <42369367+Abudfu@users.noreply.github.com>
2025-06-02 08:38:55 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,34 +14,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # shipping API
 PRODUCT_SHIPPING_API_LEVEL := 27
 
-# fscrypt policy
-TW_USE_FSCRYPT_POLICY := 1
-
-# QCOM Decryption
+# qcom decryption
 PRODUCT_PACKAGES_ENG += \
     qcom_decrypt \
     qcom_decrypt_fbe
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    libxml2 \
-    libicuuc
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so
-
-# for Android 11+ manifests
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys-intf/display
-
-# copy recovery/root/ from the common directory
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(SDM845_COMMON_PATH)/recovery/root/,$(TARGET_COPY_OUT_RECOVERY)/root/)
-
-# copy recovery/root/ from the device directory (if it exists)
-ifneq ($(wildcard $(DEVICE_PATH)/recovery/root/.),)
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root/,$(TARGET_COPY_OUT_RECOVERY)/root/)
-endif
